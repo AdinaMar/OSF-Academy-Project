5fab8149d29576ba779e0e7bd87a9bc27cde5ba2
AdinaMar <adinamartiniuc2697@gmail.com>
2022-02-13 19:34:23 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# OSF-Academy
+Done the navigation and mega-menu + login part so far. Im still focusing on getting more practice with scss. 
+To show the elements of the mega-menu of the mobile media queries, i wanted to use jquery at first(to remove and add classes) but it was getting too messy 
+so i decided to just style it using scss with check inputs and radio inputs and displaying them as 'none', 'block' or changing their visibility whenever i had
+to show/hide elements from the menu.
+I still have to make some changes regarding the media queries for tablets.
---
+++ script/app.js
@@ -0,0 +1,115 @@
+const hiddenX = document.querySelector(".fa-times");
+const showX = document.querySelector(".fa-bars");
+const megaBox = document.querySelector(".mega-box");
+const service = document.querySelector("#service");
+
+/*const showMegaBox = ()=> {
+    megaBox.style.visibility="visible";
+    megaBox.style.opacity="1";
+}
+
+service.addEventListener("click", showMegaBox);*/
+
+
+
+
+window.addEventListener("load", ()=>{
+    hiddenX.style.visibility = "hidden";
+})
+
+showX.addEventListener("click", ()=> {
+    hiddenX.style.visibility ="visible";
+})
+
+//////////////////////////////////////////////////////
+
+const pass = document.getElementById("myInput");
+const firstEye = document.getElementById("hide1");
+const secondEye = document.getElementById("hide2");
+const eyeClick = document.querySelector(".icons-eye");
+
+const showPass = ()=> {
+    if(pass.type === "password") {
+        pass.type = "text";
+        firstEye.style.display = "none";
+        secondEye.style.display = "block";
+    } else {
+       
+            pass.type = "password";
+            firstEye.style.display = "block";
+            secondEye.style.display = "none";
+    }
+}
+
+eyeClick.addEventListener("click", showPass);
+
+
+const loginForm = document.querySelector(".login-form");
+const iconUser = document.querySelector(".fa-user");
+
+
+const showLogin = ()=> {
+    loginForm.classList.toggle("show-active");
+}
+
+iconUser.addEventListener("click", showLogin);
+
+
+
+
+/////////////////////////////////////////////////////
+
+
+
+//close form by pressing 'esc'/click outside form
+
+window.addEventListener("keydown", function(event) {
+    if(event.key === "Escape") {
+        loginForm.classList.remove("show-active");
+    }
+})
+
+
+
+document.addEventListener("click", function(event) {
+    if(
+        event.target.matches(".login-form") 
+    ) {
+        loginForm.classList.remove("show-active");
+    }
+})
+
+const forCell = document.querySelector("#forCell");
+
+forCell.addEventListener("click", showLogin);
+
+////////////////////////////////////////////////// INCREMENT COUNTER/////
+const favCounter = document.querySelectorAll(".count");
+const heartShop = document.querySelector("#heartCounter");
+
+let counter= 0;
+for(let i=0; i<favCounter.length; i++){
+favCounter[i].addEventListener("click", ()=>{
+    counter+=1 ;
+    heartShop.innerHTML = counter;
+
+})
+}
+
+const shopping = document.querySelector("#counter");
+const bags = document.querySelectorAll(".shop");
+
+let counting = 0;
+for(let i=0; i<bags.length;i++){
+    bags[i].addEventListener("click", ()=>{
+        counting +=1;
+        shopping.innerHTML= counting;
+    })
+}
+
+/////////////////////////FOOTER:GET YEAR
+const thisYear = document.querySelector("#currentDate");
+const currentYear = new Date().getFullYear()
+
+
+thisYear.innerHTML = currentYear;
